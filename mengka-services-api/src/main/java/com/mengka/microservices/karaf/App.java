a63093439a88569a2fc21f9599e9452be3d881e7
hyy044101331 <hyy044101331@163.com>
2017-12-07 18:00:38 +0800

    create

---
+++ mengka-services-api/src/main/java/com/mengka/microservices/karaf/App.java
@@ -0,0 +1,13 @@
+package com.mengka.microservices.karaf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
